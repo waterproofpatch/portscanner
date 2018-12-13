8eac3974ae5ce16cb616405ac6f37982c522fcf3
waterproofpatch <waterproofpatch@gmail.com>
2018-12-12 19:11:30 -0500

    more arguments

--- main.py
+++ main.py
@@ -4,26 +4,38 @@ Port scanner
 """
 import socket
 import argparse
+import errno
 
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument("host", help="Hostname to scan")
+    argument_parser.add_argument("--timeout", type=int, default=.8, help="Timeout for each connection attempt in seconds")
+    argument_parser.add_argument("--startport", type=int, default=0, help="Starting port to scan (inclusive)")
+    argument_parser.add_argument("--endport", type=int, default=65563, help="Ending port to scan (inclusive)")
 
     args = argument_parser.parse_args()
 
     print("Scanning host {}".format(args.host))
 
     successful_connections = []
-    for port in range(65536):
+    closed_ports = []
+    # attempt a tcp connection to each port
+    for port in range(args.startport, args.endport + 1):
         print("Connecting to {}:{}".format(args.host, port))
         try:
             s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-            s.settimeout(0.8)
-            s.connect((args.host, port))
+            s.settimeout(args.timeout)
+            result = s.connect_ex((args.host, port))
+            s.close()
+            if result == 0:
+                successful_connections.append(port)
+            elif result == errno.ECONNREFUSED:
+                closed_ports.append(port)
         except OSError as exc:
             print("Unable to connect: {}".format(exc))
-            continue
-        successful_connections.append(port)
 
+    # print summary
+    for closed_port in closed_ports:
+        print("Port {}: Closed".format(closed_port))
     for successful_connection in successful_connections:
         print("Port {}: Open".format(successful_connection))
